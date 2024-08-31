1f1eb3d9920271ee1ebe74a964750238807e39c8
Luana <luanatayna2007@gmail.com>
2024-08-31 19:57:39 -0300

    Novo commit

--- app/models.py
+++ app/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
-    cadegoria = models.CharField(max_length=40)
-    descricao = models.TextField(max_length=300)
     foto = models.ImageField(blank=True)
+    texto = models.TextField(max_length=300)
 
     def __str__(self):
         return self.titulo
