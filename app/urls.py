35af47481ddc3645080b2358fa3ef97aa4f2d202
Luana <luanatayna2007@gmail.com>
2024-10-11 10:08:12 -0300

    Commit atual

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.puberdademasculina, name='puberdademasculina'),
+    path('', views.index, name='puberdademasculina'),
 ]
