d77a6cf83e2898ebb2d12cc3dfb4c7d9f2fa9e2a
Luana <luanatayna2007@gmail.com>
2024-10-11 09:53:50 -0300

    Novo commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Post
 
+def puberdademasculina (request):
+    return render(request, 'puberdademasculina.html')
+
 def index (request):
     posts = Post.objects.all()
     print(posts)
